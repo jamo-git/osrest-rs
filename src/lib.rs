d8363b29d5db330e2d4576cae501c1f1f406abf3
Jarmo <spitti.roska@gmail.com>
2022-10-06 20:38:55 +0300

    Refactored osinfo, added uptime to api

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,57 @@
 use regex::Regex;
 use rocket::serde::Serialize;
+use chrono::{offset, NaiveDateTime};
 
 #[derive(Serialize, Debug)]
-pub struct SysInfo {
+pub struct OsInfo {
     pub os_name: String,
     pub os_version: String
 }
 
-pub fn parse_windows(windata: String) -> SysInfo {
-    let mut sys_data = SysInfo { 
+#[derive(Serialize, Debug)]
+pub struct Uptime {
+    pub uptime: String,
+    pub value_type: String
+}
+
+pub fn uptime_windows(windata: String) -> Uptime {
+    let mut uptime_data: Uptime = Uptime { 
+        uptime: String::from(""), 
+        value_type: String::from("")
+    };
+    let re_boottime = Regex::new(r"(?m)^System Boot Time:\s+(.+)\r\n").unwrap();
+    let boottime = re_boottime.captures(windata.as_str())
+        .unwrap()
+        .get(1)
+        .unwrap()
+        .as_str();
+    let naivedatetime_from = NaiveDateTime::parse_from_str(boottime,
+        "%d/%m/%Y, %H.%M.%S").unwrap();
+    let nowtime = offset::Local::now().naive_local();
+    let duration = nowtime.signed_duration_since(naivedatetime_from);
+    uptime_data.uptime = duration.num_minutes().to_string();
+    uptime_data.value_type = String::from("minutes");
+    return uptime_data;
+}
+
+pub fn uptime_linux(linuxdata: String) -> Uptime {
+    let mut uptime_data: Uptime = Uptime { 
+        uptime: String::from(""), 
+        value_type: String::from("")
+    };
+
+    let data_vec: Vec<&str> = linuxdata.split(" ").collect();
+    let raw_seconds_float: f32 = data_vec[0].parse().unwrap();
+    let minutes_int: i64 = (raw_seconds_float / 60.0) as i64;
+
+    uptime_data.uptime = minutes_int.to_string();
+    uptime_data.value_type = String::from("minutes");
+
+    return  uptime_data;
+}
+
+pub fn parse_os_windows(windata: String) -> OsInfo {
+    let mut sys_data = OsInfo { 
         os_name: String::from(""),
         os_version: String::from("")
     };
@@ -29,8 +72,8 @@ pub fn parse_windows(windata: String) -> SysInfo {
     return sys_data;
 }
 
-pub fn parse_linux(linuxdata: String) -> SysInfo {
-    let mut sys_data = SysInfo { 
+pub fn parse_os_linux(linuxdata: String) -> OsInfo {
+    let mut sys_data = OsInfo { 
         os_name: String::from(""),
         os_version: String::from("")
     };
@@ -53,10 +96,18 @@ pub fn parse_linux(linuxdata: String) -> SysInfo {
     return sys_data;
 }
 
-pub fn empty_return() -> SysInfo {
-    let sys_data = SysInfo { 
+pub fn empty_return_osinfo() -> OsInfo {
+    let sys_data = OsInfo { 
         os_name: String::from("n/a"),
         os_version: String::from("n/a")
     };
     return sys_data;
+}
+
+pub fn empty_return_uptime() -> Uptime {
+    let uptime_data = Uptime { 
+        uptime: String::from("n/a"),
+        value_type: String::from("n/a")
+    };
+    return uptime_data;
 }
--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,19 @@ mod lib;
 
 #[get("/")]
 fn uri_main() -> content::RawJson<&'static str> {
-    content::RawJson("{ \"/sysinfo\": \"Check /sysinfo for System Information\" }")
+    content::RawJson("{ \"/osinfo\": \"Check /osinfo for System Information\", \"/uptime\": \"Check /uptime for Uptime\" }")
 }
 
 #[get("/")]
-fn sysinfo() -> Json<lib::SysInfo> {
+fn osinfo() -> Json<lib::OsInfo> {
     if cfg!(target_os = "windows") {
         let output = Command::new("cmd")
                 .args(["/C", "systeminfo"])
                 .output()
                 .expect("Failed to check systeminfo");
         let output_string = String::from_utf8(output.stdout).expect("Not valid utf-8");
-        let sys_data = lib::parse_windows(output_string);
-        Json(sys_data)
+        let os_data = lib::parse_os_windows(output_string);
+        Json(os_data)
     }
     else if cfg!(target_os = "linux") {
         let output = Command::new("sh")
@@ -30,11 +30,37 @@ fn sysinfo() -> Json<lib::SysInfo> {
                 .output()
                 .expect("Failed to check os-release");
         let output_string = String::from_utf8(output.stdout).expect("Not valid utf-8");
-        let sys_data = lib::parse_linux(output_string);
-        Json(sys_data)
+        let os_data = lib::parse_os_linux(output_string);
+        Json(os_data)
     } 
     else {
-        Json(lib::empty_return())
+        Json(lib::empty_return_osinfo())
+    }
+}
+
+#[get("/")]
+fn uptime() -> Json<lib::Uptime> {
+    if cfg!(target_os = "windows") {
+        let output = Command::new("cmd")
+                .args(["/C", "systeminfo"])
+                .output()
+                .expect("Failed to check systeminfo");
+        let output_string = String::from_utf8(output.stdout).expect("Not valid utf-8");
+        let uptime_data = lib::uptime_windows(output_string);
+        Json(uptime_data)
+    }
+    else if cfg!(target_os = "linux") {
+        let output = Command::new("sh")
+                .arg("-c")
+                .arg("cat /proc/uptime")
+                .output()
+                .expect("Failed to check os-release");
+        let output_string = String::from_utf8(output.stdout).expect("Not valid utf-8");
+        let os_data = lib::uptime_linux(output_string);
+        Json(os_data)
+    } 
+    else {
+        Json(lib::empty_return_uptime())
     }
 }
 
@@ -43,5 +69,6 @@ fn sysinfo() -> Json<lib::SysInfo> {
 fn rocket() -> _ {
     rocket::build()
     .mount("/", routes![uri_main])
-    .mount("/sysinfo", routes![sysinfo])
+    .mount("/osinfo", routes![osinfo])
+    .mount("/uptime", routes![uptime])
 }
